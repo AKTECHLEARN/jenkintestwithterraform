5c07a307543d05253ece64494dfcec9c03f75a9b
AKTECHLEARN <AKTECHLEARN@GMAIL.COM>
2025-08-08 17:43:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
-# main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.111.0"
+    }
+  }
+}
 
 provider "azurerm" {
-  features = {}
+  features        = {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
@@ -20,4 +27,3 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.example.location
 }
-

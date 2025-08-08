f912fb01b5ef05246174ee5cc0e40837db8aef92
AKTECHLEARN <AKTECHLEARN@GMAIL.COM>
2025-08-08 17:43:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-# variables.tf
-
 variable "resource_group_name" {
   description = "The name of the Azure Resource Group"
   type        = string
@@ -15,24 +13,19 @@ variable "location" {
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
-  default     = "61c93cef-3378-4258-b3f6-98ce21be82e9"
 }
 
 variable "client_id" {
   description = "Azure Service Principal Client ID"
   type        = string
-  default     = "bba2e01f-0279-46bd-ac9d-6222db327360"
 }
 
 variable "client_secret" {
   description = "Azure Service Principal Client Secret"
   type        = string
-  default     = "h6Z8Q~~7P66AMkVFa~b7hpTB3bHGUtovifkTBat4"
 }
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
   type        = string
-  default     = "bf7f6c35-a901-4467-b2ac-46fff520f3fa"
 }
-
